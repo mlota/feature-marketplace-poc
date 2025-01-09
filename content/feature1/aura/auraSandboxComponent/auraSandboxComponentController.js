66286ec4513c87dad01d33ac85e7f0f6b0acc940
Mel Lota <melvin_lota@hotmail.com>
2025-01-09 14:29:46 +0000

    [chore]: Adds sample aura component bundle

---
+++ content/feature1/aura/auraSandboxComponent/auraSandboxComponentController.js
@@ -0,0 +1,6 @@
+({
+	// Example of a simple controller function
+	doInit: function (component, event, helper) {
+		console.log('Aura Component Initialized');
+	},
+});
---
+++ content/feature1/aura/auraSandboxComponent/auraSandboxComponentHelper.js
@@ -0,0 +1,6 @@
+({
+	// Example of a simple helper function
+	helperMethod: function () {
+		// Add helper logic if needed
+	},
+});
